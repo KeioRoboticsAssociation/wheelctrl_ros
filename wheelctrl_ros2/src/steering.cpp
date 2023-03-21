b79bd0a8f90121d645ff6c3ef481d4253d90a1a5
long-paddy-field <84272166+long-paddy-field@users.noreply.github.com>
2023-03-21 17:23:58 +0900

    FIX: atan2 の引数の修正

--- wheelctrl_ros2/src/steering.cpp
+++ wheelctrl_ros2/src/steering.cpp
@@ -55,12 +55,12 @@ void illias::MeasureSteering::cal_disp(std::vector<float> encoder, float imu,
 
   if (!is_transformed) {
     for (int i = 0; i < 4; i++) {
-      delta.w = 0.25 * neo_atan2(r_x[i], r_y[i]) / u_param.wheels[i].distance;
+      delta.w = 0.25 * neo_atan2(r_y[i], r_x[i]) / u_param.wheels[i].distance;
     }
   } else {
     for (int i = 0; i < 4; i++) {
       delta.w =
-          0.25 * neo_atan2(r_x[i], r_y[i]) / u_param.wheels[i + 4].distance;
+          0.25 * neo_atan2(r_y[i], r_x[i]) / u_param.wheels[i + 4].distance;
     }
   }
 
